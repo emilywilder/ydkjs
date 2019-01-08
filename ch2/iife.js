a57cb33674cb49f8d4731a5369acfca46e79cf25
Emily Wilder <emilywilder.earth@gmail.com>
2019-01-08 16:16:09 -0500

    semicolon changes

--- ch2/iife.js
+++ ch2/iife.js
@@ -1,11 +1,12 @@
 function foo() {
-    a = 10;
-    console.log(a)
-};
+    let a = 10;
+    console.log(a);
+}
 
 foo();
 
 (function foo() {
-    a = 20;
-    console.log(a)
+    let a = 20;
+    console.log(a);
 })();
+
