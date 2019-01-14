f8228f898d9473185c4de9ba0a6ca4049d1846e2
Emily Wilder <emilywilder.earth@gmail.com>
2019-01-14 12:02:42 -0500

    demonstrate using let

--- closure.js
+++ closure.js
@@ -21,5 +21,17 @@ function with_closure() {
     }
 }
 
+// using let uses block scoping and closures
+function with_let() {
+    console.log("Using let...")
+    for (let i = 1; i <= 5; i++) {
+        setTimeout(function timer() {
+            console.log(i);
+        }, i * 1000);
+    }
+}
+
 without_closure()
 setTimeout(with_closure, 6000)
+setTimeout(with_let, 12000)
+
