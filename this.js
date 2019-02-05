95733ea3899ac687d0e3c56a9c3408fac4ed190f
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-05 15:50:45 -0500

    add demonstration of new scoping

--- this.js
+++ this.js
@@ -2,8 +2,13 @@ function foo() {
     console.log( this.a );
 }
 
+function bar(x) {
+    this.a = x;
+    this.foo = foo;
+}
+
 a = 0;
-var obj1 = { a: 1, foo: foo };
+var obj1 = { a: 1, foo: foo, bar: bar };
 var obj2 = { a: 2 };
 var obj3 = { a: 3 };
 
@@ -17,6 +22,15 @@ console.log("explicit binding...");
 obj1.foo.call( obj2 );
 
 console.log("hard binding...");
-var bar = obj1.foo.bind(obj3);
-bar();
+(obj1.foo.bind(obj3))();
+
+console.log("new binding...");
+obj1.bar(1);
+var obj4 = new obj1.bar(4);
+console.log( "obj1.a: " + obj1.a );
+console.log( "obj4.a: " + obj4.a );
+
+
+
+
 
