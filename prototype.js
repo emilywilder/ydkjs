ff3201517a8fb8d5378224fe6c652cb552d22b40
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-27 14:31:51 -0500

    add display of shadowing

--- prototype.js
+++ prototype.js
@@ -4,6 +4,18 @@ function log(msg) {
 
 log("creating anotherObj with property a");
 
+function printProperties() {
+    log("Properties of anotherObject");
+    console.log(Object.getOwnPropertyNames(anotherObj));
+    log("Properties of amyObject");
+    console.log(Object.getOwnPropertyNames(myObj));
+
+    log("iterating over properties of myObj");
+    for (k in myObj) {
+        console.log("found: " + k)
+    }
+}
+
 var anotherObj = {
     a: 1
 };
@@ -13,11 +25,16 @@ var myObj = Object.create( anotherObj );
 log("setting property b to myObj");
 myObj.b = 2;
 
-log("iterating over properties of myObj");
-for (k in myObj) {
-    console.log("found: " + k)
-}
+printProperties();
 
 log("using in to check properties of myObj");
 console.log("a in myObj?: " + ("a" in myObj));
 console.log("b in myObj?: " + ("b" in myObj));
+
+log("Adding/shadowing a to myObj");
+myObj.a = 3;
+console.log("anotherObj.a = " + anotherObj.a);
+console.log("myObj.a = " + myObj.a);
+
+printProperties();
+
