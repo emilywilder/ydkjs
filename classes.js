9f77238cba95528850cc7c8d68d727be6ce57796
Emily Wilder <emilywilder.earth@gmail.com>
2019-03-04 14:48:11 -0500

    demonstrate setting a function prototype to another

--- classes.js
+++ classes.js
@@ -1,7 +1,7 @@
 const my = require("./my.js");
 
 function Foo(name) {
-    this.name = name
+    this.name = name;
 }
 
 let a = new Foo("a");
@@ -31,3 +31,37 @@ Object.defineProperty(b, "constructor", {
 
 my.log("Is b's constructor Foo?: ", false);
 console.log(b.constructor === Foo);
+
+
+my.log("Demonstrating a linked 'inheritance' between functions");
+my.log("Crating Name class with name attr");
+function Name(name) {
+    this.name = name
+}
+
+Name.prototype.getName = function() {
+    return this.name;
+};
+
+my.log("Crating NameAndAge class that calls Name and has age attr");
+function NameAndAge(name, age) {
+    Name.call( this, name );
+    this.age = age
+}
+
+my.log("Setting NameAndAge prototype to be a copy of Name's");
+Object.setPrototypeOf( NameAndAge.prototype, Name.prototype );
+// Another way to do this without ES6 function:
+// NameAndAge.prototype = Object.create( Name.prototype );
+
+NameAndAge.prototype.getAge = function() {
+    return this.age;
+};
+
+my.log("Creating NameAndAge as c");
+let c = new NameAndAge("Emily", 29);
+
+my.log("c's name: ", false);
+console.log(c.getName());
+my.log("c's age: ", false);
+console.log(c.getAge());
